aed88e7e442dbdf289fb7a5ecae9cae0f1ad3916
LaVon Mejia <lavonmejia@gmail.com>
2019-09-05 23:05:14 -0400

    gitignore added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express'); // import the express package
 
 const server = express(); // creates the server
+
+const port = process.env.PORT;
 
 // handle requests to the root of the api, the / route
 server.get('/', (req, res) => {
@@ -24,5 +28,5 @@ server.get('/verses', (req, res) => {
 
 // watch for connections on port 5000
 server.listen(5000, () =>
-  console.log('Server running on http://localhost:5000')
+  console.log(`Server running on http://localhost:${port}`)
 );
